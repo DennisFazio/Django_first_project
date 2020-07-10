242fc0d83621d0b74eacb372fd0d963b5531069c
Dennis Fazio <fazioferreira@gmail.com>
2020-07-10 08:13:09 -0300

    First Commit

---
+++ vmdjango1/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\dennis\pyhton_project\curso_django\django1\vmdjango1\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
